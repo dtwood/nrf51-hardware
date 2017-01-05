473a288147a46821478951afb7bb34960616742f
David Wood <devel@dtwood.uk>
2017-01-05 01:24:35 +0000

    Run rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 #![no_std]
 #![feature(asm)]
 
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
 extern crate nrf51_atomic;
 extern crate spin;
 extern crate volatile_register;
